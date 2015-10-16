7cc83e6bb65963af30eeb88577f7b45caf007f6d
parrt <parrt@cs.usfca.edu>
2015-10-16 16:51:39 -0700

    put COPY_ON_WRITE back in

--- wlib/src/refcounting.h
+++ wlib/src/refcounting.h
@@ -81,4 +81,11 @@ static inline void DEREF(void *x) {
 	}
 }
 
+static inline void COPY_ON_WRITE(void *x) {
+//	if ( x!=NULL && ((heap_object *)x)->refs > 1 ) {
+//		((heap_object *)x)->refs--;
+//		x = Vector_copy(x);
+//		((heap_object *)x)->refs = 1;
+//	}
+}
 #endif
