e05b8a6257cbf76cc7d32f941faf9227289baa33
hanjoes <hanzhou87@gmail.com>
2015-10-23 16:27:21 -0700

    Fixed path issue in build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ fi
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 # make install the project
-BUILD_DIR=/tmp/wich-build
-TEST_DIR=/tmp/cunit
+BUILD_DIR=/tmp/wich-build/
+CUNIT_DIR=$BUILD_DIR/malloc/cunit/
 
 function install {
     rm -rf $BUILD_DIR
@@ -22,10 +22,12 @@ function install {
 
 function test {
     rm -rf $BUILD_DIR && mkdir $BUILD_DIR 2>/dev/null
-    rm -rf $TEST_DIR && mkdir $TEST_DIR 2>/dev/null
-
     cd $BUILD_DIR && cmake $DIR >/dev/null  && make >/dev/null
-    cp $DIR/cunit/addr2index.py $DIR/cunit/ANSIC_MALLOC_FREE_TRACE.txt $BUILD_DIR/cunit
+    
+    rm -rf $CUNIT_DIR && mkdir $CUNIT_DIR 2>/dev/null    
+    cp $DIR/cunit/addr2index.py $CUNIT_DIR
+    cp $DIR/cunit/ANSIC_MALLOC_FREE_TRACE.txt $CUNIT_DIR
+    
     cd $BUILD_DIR && ctest ./
 }
 
